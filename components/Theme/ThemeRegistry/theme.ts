b13ab2757229226721bd0f119ae1af3dce313020
Gaëtan DUHAMEL <gaetan_duhamel@natural-solutions.eu>
2023-06-04 02:56:43 +0200

    fix: adapt theme registry to Storybook

--- components/Theme/ThemeRegistry/theme.ts
+++ components/Theme/ThemeRegistry/theme.ts
@@ -2,7 +2,7 @@
 
 import { createTheme, ThemeOptions } from "@mui/material/styles";
 
-import GoogleInterFont from "@/app/fonts/fonts";
+import GoogleInterFont from "../../../app/fonts/fonts";
 
 // When needed::: first argument is needed if you have common enterprise theme, and second argument is to override your enterprise theme.
 // apply fonts to all other typography options like headings, subtitles, etc...
